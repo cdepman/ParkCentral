4033f330632a4743256d823b9ee9f6b97969fcb0
Charlie Depman <cdepman@gmail.com>
2023-07-02 11:11:45 -0400

    update config with eas project id

--- app.config.js
+++ app.config.js
@@ -1,7 +1,16 @@
 module.exports = {
-  name: 'ParkFinder',
-  version: '1.0.0',
+  name: "ParkFinder",
+  version: "1.0.0",
   credentials: {
     googleMapsApiKey: process.env.API_URL,
+  },
+  extra: {
+    eas: {
+      projectId: "de469ced-e00e-4412-b2f9-e691b3bf3ae3",
+    },
+  },
+  ios: {
+    bundleIdentifier: "com.madpen.parkcentral",
+    buildNumber: "1.0.0",
   },
 };
