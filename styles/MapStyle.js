3256fb21cbf995a2f32dcc5854273244559217b6
Charlie Depman <cdepman@gmail.com>
2023-07-04 09:16:57 -0400

    update map style to focus more on parks, highlight park paths

--- styles/MapStyle.js
+++ styles/MapStyle.js
@@ -1,22 +1,7 @@
 export const MapStyle = [
   {
-    featureType: "landscape.man_made",
-    stylers: [
-      {
-        visibility: "off",
-      },
-    ],
-  },
-  {
-    featureType: "road.arterial",
-    stylers: [
-      {
-        visibility: "off",
-      },
-    ],
-  },
-  {
-    featureType: "road.highway",
+    featureType: "administrative",
+    elementType: "labels.text",
     stylers: [
       {
         visibility: "off",
@@ -32,6 +17,14 @@ export const MapStyle = [
       },
       {
         visibility: "on",
+      },
+    ],
+  },
+  {
+    featureType: "landscape.man_made",
+    stylers: [
+      {
+        visibility: "off",
       },
     ],
   },
@@ -74,6 +67,15 @@ export const MapStyle = [
       },
       {
         visibility: "on",
+      },
+    ],
+  },
+  {
+    featureType: "poi",
+    elementType: "labels.text",
+    stylers: [
+      {
+        visibility: "off",
       },
     ],
   },
@@ -133,7 +135,40 @@ export const MapStyle = [
         color: "#149974",
       },
       {
+        visibility: "off",
+      },
+      {
         weight: 3.5,
+      },
+    ],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "labels.text",
+    stylers: [
+      {
+        color: "#215037",
+      },
+      {
+        visibility: "on",
+      },
+      {
+        weight: 0.5,
+      },
+    ],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        color: "#000000",
+      },
+      {
+        visibility: "on",
+      },
+      {
+        weight: 0.5,
       },
     ],
   },
@@ -147,6 +182,74 @@ export const MapStyle = [
   },
   {
     featureType: "poi.sports_complex",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    elementType: "labels.icon",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "road.arterial",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "road.highway",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "road.local",
+    stylers: [
+      {
+        visibility: "on",
+      },
+      {
+        weight: 2.5,
+      },
+    ],
+  },
+  {
+    featureType: "road.local",
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        color: "#ffffff",
+      },
+      {
+        visibility: "on",
+      },
+      {
+        weight: 1,
+      },
+    ],
+  },
+  {
+    featureType: "road.local",
+    elementType: "geometry.stroke",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "transit",
     stylers: [
       {
         visibility: "off",
